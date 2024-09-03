06b557095b864bb4381fb5ec2db80e28c0b68e4d
isimsek7 <isimsekmusic@gmail.com>
2024-09-04 02:11:18 +0300

    Add files via upload

---
+++ Classes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace linqJoin;
+public class Author
+{
+    public int AuthorId { get; set; } 
+    public string Name { get; set; }   
+}
+
+public class Book
+{
+    public int BookId { get; set; }
+    public string Title { get; set; }  
+    public int AuthorId { get; set; } 
+}
+
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using linqJoin;
+
+List<Author> authors = new List<Author>
+        {
+            new Author { AuthorId = 1, Name = "Orhan Pamuk" },
+            new Author { AuthorId = 2, Name = "Elif Şafak" },
+            new Author { AuthorId = 3, Name = "Ahmet Ümit" }
+        };
+
+// Create the list of books
+List<Book> books = new List<Book>
+        {
+            new Book { BookId = 1, Title = "Kar", AuthorId = 1 },
+            new Book { BookId = 2, Title = "İstanbul", AuthorId = 1 },
+            new Book { BookId = 3, Title = "10 Minutes 38 Seconds in This Strange World", AuthorId = 2 },
+            new Book { BookId = 4, Title = "Beyoğlu Rapsodisi", AuthorId = 3 }
+        };
+
+
+var query = from book in books
+            join author in authors on book.AuthorId equals author.AuthorId
+            select new
+            {
+                BookTitle = book.Title,
+                AuthorName = author.Name
+            };
+foreach (var item in query)
+{
+    Console.WriteLine($"Book: {item.BookTitle}, Author: {item.AuthorName}");
+}
